0541cbdaa1bfe7ee1a74cf43797529f039fbe23c
Humberto Calero <altairn5@uga.edu>
2016-05-19 16:00:34 -0700

    html

---
+++ controllers/pricesController.js
@@ -0,0 +1,34 @@
+
+/*Prices Controllers*/
+
+function meau(req, res){
+
+	console.log('prices are here');
+
+}
+
+function create(req, res){
+
+}
+
+function show(req, res){
+
+}
+
+function destroy(req, res){
+
+}
+
+function update(req, res){
+
+}
+
+
+module.exports = {
+
+	index: meau,
+	create: create,
+	show: show,
+	destroy: destroy,
+	update: update
+}
--- server.js
+++ server.js
@@ -3,13 +3,49 @@ var express = require('express'),
 bodyParser = require('body-parser'),
 path = require('path'),
 controllers = require('./controllers'),
+hbs = require('express-handlebars');
 app = express();
 
 /*Configuration*/
 
+
 /*Port*/
 
 app.set('port', (process.env.PORT||3000));
+
+
+/*View Engine*/
+app.engine('handlebars', hbs({default: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+/*Dependecies*/
+
+app.use('/static', express.static(__dirname + '/public'));
+app.use('/vendor', express.static(__dirname + '/bower_components'));
+
+/*Body Parser*/
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+app.get(['/', '/index', '/show'], function (req, res){
+
+
+	// res.sendFile(__dirname  + '/views/index.html');
+	   res.render('main', {salutation : "Hello There", appName: "Rideprice"})
+
+});
+
+app.get('/prices', controllers.prices.index);
+
 
 
 /**********
